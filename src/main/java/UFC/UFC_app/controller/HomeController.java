40545c04acc915483cf3f773571cbe47b120cb3b
gonsva <gonsva@gmail.com>
2019-06-27 11:26:41 +0100

    added a method for the homepage

--- src/main/java/UFC/UFC_app/controller/HomeController.java
+++ src/main/java/UFC/UFC_app/controller/HomeController.java
@@ -1,5 +1,14 @@
 package UFC.UFC_app.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	
+	@RequestMapping("/") //this annotation is for specifying the url that will then trigger this method
+	public String home() {
+		return "Hello everyone";
+	}
 
 }
