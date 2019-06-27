1c63a5f1c98fd78973a658edad09c29fb2439569
gonsva <gonsva@gmail.com>
2019-06-27 10:51:58 +0100

    initial project structure and gitignore file

---
+++ src/main/java/UFC/UFC_app/App.java
@@ -0,0 +1,13 @@
+package UFC.UFC_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
