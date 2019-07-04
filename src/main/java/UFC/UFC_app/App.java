ca1ac773375d347757367577b0e0a0da5dbed053
gonsva <gonsva@gmail.com>
2019-07-04 12:16:53 +0100

    UFC Backend

--- src/main/java/UFC/UFC_app/App.java
+++ src/main/java/UFC/UFC_app/App.java
@@ -1,6 +1,9 @@
 package UFC.UFC_app;
 
 import org.springframework.boot.SpringApplication;
+
+//Spring Boot: Java-based framework, enables you to develop enterprise-ready applications that you can “just run”.
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/UFC/UFC_app/repository/NotesRepository.java
+++
@@ -1,9 +0,0 @@
-package UFC.UFC_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import UFC.UFC_app.model.Note;
-
-public interface NotesRepository 
-		extends JpaRepository<Note, Long>{
-
-}
